11162a06961ca47a6de893cc787ea555894fe23a
Vitor Monteiro <mluizvitor@gmail.com>
2023-01-30 16:14:27 -0300

    Updated Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,21 +1,24 @@
-import { Plus } from 'phosphor-react'
-import { ButtonHTMLAttributes, ComponentPropsWithoutRef, ElementType, HTMLProps, ReactNode } from 'react';
+import { Plus } from "phosphor-react"
+import { ButtonHTMLAttributes, ComponentPropsWithoutRef, ElementType, HTMLProps, ReactNode } from "react";
 
-import styles from './styles.module.css';
+import styles from "./styles.module.css";
 
 interface ButtonProps extends ComponentPropsWithoutRef<ElementType> {
   label: string;
   icon?: ReactNode;
   className?: string;
+  hideLabel?: boolean;
 }
 
-export default function Button({ label, icon, className, ...rest }: ButtonProps) {
+export default function Button({ label, icon, className, hideLabel, ...rest }: ButtonProps) {
   return (
     <button className={[styles.buttonHexagon, className].join(" ")} {...rest}>
       {icon && icon}
-      <span className={icon ? 'ml-3' : ""}>
-        {label}
-      </span>
+      {!hideLabel && (
+        <span className={icon ? "ml-3" : ""}>
+          {label}
+        </span>
+      )}
     </button>
   )
 }
