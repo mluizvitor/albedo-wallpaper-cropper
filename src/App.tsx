fe34df607770f5c33e0bc44a9e056ad4640f1273
Vitor Monteiro <mluizvitor@gmail.com>
2023-01-31 16:15:39 -0300

    Moved Loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
-import { useCanvas } from "./hooks/useCanvas"
+import { CropperSection } from './Sections/CropperSection';
+import { CanvasSection } from './Sections/CanvasSection';
+import FileListSection from './Sections/FileListSection';
 
-import { CropperSection } from "./Sections/CropperSection";
-import { CanvasSection } from "./Sections/CanvasSection";
-import FileListSection from "./Sections/FileListSection";
-
+import IDB from './database/config';
+import { useCanvas } from './hooks/useCanvas';
+import { CircleNotch } from 'phosphor-react';
 
 function App() {
+  const { showLoader } = useCanvas();
+
+  IDB();
+
   return (
     <main>
       <CanvasSection />
       <CropperSection />
       <FileListSection />
+      {showLoader && (
+        <div className='fixed inset-0 bg-neutral-900/50 flex items-center justify-center backdrop-blur-lg z-50'>
+          <CircleNotch size={96}
+            className='animate-spin' />
+        </div>
+      )}
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
